4759e5dcf69c334f75de10af1673e76a891728b3
Julian Pustkuchen <Julian@Pustkuchen.com>
2014-06-24 09:29:21 +0200

    Now uses .bind and Drupals context variable instead of the jQuery .on() event binder to be better compatible with older versions of jQuery. Based on the issue [#2291285] by [@gooddesignusa]: Uncaught TypeError: undefined is not a function.

--- colorbox_swipe.js
+++ colorbox_swipe.js
@@ -5,31 +5,25 @@
   Drupal.behaviors.colorbox_swipe = {
     /**
      * Run Drupal module JS initialization.
-     * 
+     *
      * @param context
      * @param settings
      */
-    attach : function(context, settings) {
+    attach: function(context, settings) {
       // Global context!
-      $(document)
-          .ready(
-              function() {
-                $("#colorbox *")
-                    .on(
-                        'swipeleft',
-                        function(e) {
-                          // Disable selection.
-                          $(this).disableSelection();
-                          $.colorbox.next();
-                        })
-                    .on(
-                        'swiperight',
-                        function(e) {
-                          // Disable selection.
-                          $(this).disableSelection();
-                          $.colorbox.prev();
-                        });
-              });
+      $("#colorbox *", context).bind(
+        'swipeleft',
+        function(e) {
+          // Disable selection.
+          $(this).disableSelection();
+          $.colorbox.next();
+        }).bind(
+        'swiperight',
+        function(e) {
+          // Disable selection.
+          $(this).disableSelection();
+          $.colorbox.prev();
+        });
     }
   }
 })(jQuery);
